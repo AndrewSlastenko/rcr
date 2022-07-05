7bba71a77aedfa354afa2ff41f9f0ab50d4c38d9
SeaDoge <and.slastenko@gmail.com>
2022-07-05 19:42:36 +0700

    Start project

---
+++ src/main/java/org/slastenko/rcr/RcrApplication.java
@@ -0,0 +1,13 @@
+package org.slastenko.rcr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RcrApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RcrApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/slastenko/rcr/RcrApplicationTests.java
@@ -0,0 +1,13 @@
+package org.slastenko.rcr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RcrApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
